dc0eba051a0aa5531c3615ad15366012e0931de4
ascandone <alescandone@gmail.com>
2024-07-13 12:26:43 +0200

    added comparation functionality to position and range

--- parser/ast.go
+++ parser/ast.go
@@ -5,9 +5,22 @@ type Position struct {
 	Line      int
 }
 
+func (p1 *Position) GtEq(p2 Position) bool {
+	if p1.Line == p2.Line {
+		return p1.Character >= p2.Character
+	}
+
+	return p1.Line > p2.Line
+}
+
 type Range struct {
 	Start Position
 	End   Position
+}
+
+func (r *Range) Contains(position Position) bool {
+	//  position >= r.Start && r.End >= position
+	return position.GtEq(r.Start) && r.End.GtEq(position)
 }
 
 // does it even make sense to have a literal supertype?
