50bafb767ddf366c37215c29a2a8c6cab2592cf6
ascandone <alescandone@gmail.com>
2024-07-17 09:51:05 +0200

    split fault tolerance parsing tests in different file

---
+++ parser/parser_fault_tolerance_test.go
@@ -0,0 +1,67 @@
+package parser_test
+
+import (
+	"numscript/parser"
+	"testing"
+
+	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFaultToleranceVarName(t *testing.T) {
+	p := parser.Parse(`vars { monetary 42  }`)
+	snaps.MatchSnapshot(t, p.Value)
+	assert.NotEmpty(t, p.Errors)
+}
+
+func TestFaultToleranceSend(t *testing.T) {
+	p := parser.Parse(`send `)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceMonetary(t *testing.T) {
+	p := parser.Parse(`send [COIN]`)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceNoAddr(t *testing.T) {
+	p := parser.Parse(`send  (
+  source = {
+	@
+  }
+  destination = @
+)`)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceInvalidDest(t *testing.T) {
+	p := parser.Parse(`send [COIN 10] (
+    source = @a
+    destination =
+)`)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceInvalidSrcTk(t *testing.T) {
+	p := parser.Parse(`send [COIN 10] (
+    source = max
+    destination = @d
+)`)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceTrailingComma(t *testing.T) {
+	p := parser.Parse(`set_tx_meta(1, )`)
+	snaps.MatchSnapshot(t, p.Value)
+}
+
+func TestFaultToleranceDestinationNoRemaining(t *testing.T) {
+	p := parser.Parse(`send [COIN 10] (
+		source = @a
+		destination = {
+			max [COIN 10] from @x
+		}
+	)
+	`)
+	snaps.MatchSnapshot(t, p.Value)
+}
