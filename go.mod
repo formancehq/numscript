2c9b95e2c2578d403997c0fb6e293b38bdd75458
Alessandro Scandone <alescandone@gmail.com>
2024-07-24 17:17:41 +0200

    Setup sentry (#1)

--- go.mod
+++ go.mod
@@ -10,10 +10,13 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
+	github.com/getsentry/sentry-go v0.28.1
 	github.com/gkampitakis/ciinfo v0.3.0 // indirect
 	github.com/gkampitakis/go-diff v1.3.2 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
--- main.go
+++ main.go
@@ -6,11 +6,39 @@ import (
 	lsp "numscript/lsp"
 	"numscript/parser"
 	"os"
+	"runtime/debug"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 )
+
+// This has to be set dynamically via the following flag:
+// -ldflags "-X main.Version=0.0.1"
+var Version string = "develop"
 
 var pathFlag = flag.String("path", "", "path to execute")
 
+func recoverPanic() {
+	r := recover()
+	if r == nil {
+		return
+	}
+
+	errMsg := fmt.Sprintf("[uncaught panic]@v%s: %s\n%s\n", Version, r, string(debug.Stack()))
+	os.Stderr.Write([]byte(errMsg))
+	sentry.CaptureMessage(errMsg)
+	sentry.Flush(2 * time.Second)
+}
+
 func main() {
+	sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://b8b6cfd5dab95e1258d80963c3db73bf@o4504394442539008.ingest.us.sentry.io/4507623538884608",
+		AttachStacktrace: true,
+	})
+
+	defer recoverPanic()
+	defer sentry.Flush(2 * time.Second)
+
 	flag.Parse()
 	firstArg := flag.Arg(0)
 
@@ -22,6 +50,9 @@ func main() {
 		})
 		return
 
+	case "version":
+		fmt.Printf("numscript %s\n", Version)
+
 	case "parse":
 		if *pathFlag == "" {
 			fmt.Println("Err: Path argument is required")
@@ -30,7 +61,8 @@ func main() {
 
 		dat, err := os.ReadFile(*pathFlag)
 		if err != nil {
-			panic(err)
+			os.Stderr.Write([]byte(err.Error()))
+			return
 		}
 
 		parsed := parser.Parse(string(dat))
@@ -41,6 +73,6 @@ func main() {
 		}
 		return
 	default:
-		fmt.Printf("Invalid argument: '%s'", firstArg)
+		os.Stderr.Write([]byte(fmt.Sprintf("Invalid argument: '%s'", firstArg)))
 	}
 }
