5555e4afd905d89bd7631237b8032413172559b6
ascandone <alescandone@gmail.com>
2024-07-24 16:43:00 +0200

    logging errors to sentry

--- main.go
+++ main.go
@@ -6,11 +6,35 @@ import (
 	lsp "numscript/lsp"
 	"numscript/parser"
 	"os"
+	"runtime/debug"
+	"time"
+
+	"github.com/getsentry/sentry-go"
 )
 
 var pathFlag = flag.String("path", "", "path to execute")
 
+func recoverPanic() {
+	r := recover()
+	if r == nil {
+		return
+	}
+
+	errMsg := fmt.Sprintf("[uncaught panic]: %s\n%s\n", r, string(debug.Stack()))
+	os.Stderr.Write([]byte(errMsg))
+	sentry.CaptureMessage(errMsg)
+	sentry.Flush(2 * time.Second)
+}
+
 func main() {
+	sentry.Init(sentry.ClientOptions{
+		Dsn:              "https://b8b6cfd5dab95e1258d80963c3db73bf@o4504394442539008.ingest.us.sentry.io/4507623538884608",
+		AttachStacktrace: true,
+	})
+
+	defer recoverPanic()
+	defer sentry.Flush(2 * time.Second)
+
 	flag.Parse()
 	firstArg := flag.Arg(0)
 
@@ -30,7 +54,8 @@ func main() {
 
 		dat, err := os.ReadFile(*pathFlag)
 		if err != nil {
-			panic(err)
+			os.Stderr.Write([]byte(err.Error()))
+			return
 		}
 
 		parsed := parser.Parse(string(dat))
@@ -41,6 +66,6 @@ func main() {
 		}
 		return
 	default:
-		fmt.Printf("Invalid argument: '%s'", firstArg)
+		os.Stderr.Write([]byte(fmt.Sprintf("Invalid argument: '%s'", firstArg)))
 	}
 }
