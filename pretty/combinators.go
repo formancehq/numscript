986770b6fec1e9f1d51304a14a04068d9f14b701
ascandone <alescandone@gmail.com>
2024-08-07 12:11:54 +0200

    implemented the easy part

---
+++ pretty/combinators.go
@@ -0,0 +1,23 @@
+package pretty
+
+func Text(t string) Document {
+	return text{Text: t}
+}
+
+func Concat(docs ...Document) Document {
+	return concat{Documents: docs}
+}
+
+func Empty() Document {
+	return concat{}
+}
+
+// l is the number of empty lines, not the number of the newlines
+// therefore there are l + 1 newlines
+func Lines(l uint) Document {
+	return lines{Lines: l}
+}
+
+func Nest(d Document) Document {
+	return nest{Document: d}
+}
