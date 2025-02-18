b1259d0a2040a58a6251b26403f71fc492c91347
ascandone <alescandone@gmail.com>
2025-02-18 15:41:40 +0100

    chore: added jsonrpc server test

---
+++ internal/jsonrpc2/jsonrpc2_test.go
@@ -0,0 +1,34 @@
+package jsonrpc2_test
+
+import (
+	"testing"
+
+	"github.com/formancehq/numscript/internal/jsonrpc2"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandleRequest(t *testing.T) {
+	type SumParams struct {
+		X int `json:"x"`
+		Y int `json:"y"`
+	}
+
+	in := make(chan jsonrpc2.Message)
+	out := make(chan jsonrpc2.Message)
+
+	server := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(in, out))
+	jsonrpc2.HandleRequest(server, "sum", func(p SumParams) any {
+		return p.X + p.Y
+	})
+	go server.Listen()
+	defer server.Close()
+
+	client := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(out, in))
+	go client.Listen()
+	defer server.Close()
+
+	res, err := jsonrpc2.SendRequest[int](client, "sum", SumParams{X: 100, Y: 42})
+	require.Nil(t, err)
+
+	require.Equal(t, 142, *res)
+}
