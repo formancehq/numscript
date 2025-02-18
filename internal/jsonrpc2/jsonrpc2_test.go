b707380485f60efc7e84310e108b8cff6355e8b0
ascandone <alescandone@gmail.com>
2025-02-18 16:13:20 +0100

    refactor: extracted test utility

--- internal/jsonrpc2/jsonrpc2_test.go
+++ internal/jsonrpc2/jsonrpc2_test.go
@@ -13,20 +13,28 @@ func TestHandleRequest(t *testing.T) {
 		Y int `json:"y"`
 	}
 
-	in := make(chan jsonrpc2.Message)
-	out := make(chan jsonrpc2.Message)
+	server, client := newClientServer()
 
-	server := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(in, out))
 	jsonrpc2.HandleRequest(server, "sum", func(p SumParams) any {
 		return p.X + p.Y
 	})
-	go server.Listen()
-
-	client := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(out, in))
-	go client.Listen()
 
 	res, err := jsonrpc2.SendRequest[int](client, "sum", SumParams{X: 100, Y: 42})
 	require.Nil(t, err)
 
 	require.Equal(t, 142, *res)
+}
+
+func newClientServer() (*jsonrpc2.Server, *jsonrpc2.Server) {
+	in := make(chan jsonrpc2.Message)
+	out := make(chan jsonrpc2.Message)
+
+	server := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(in, out))
+
+	client := jsonrpc2.NewServer(jsonrpc2.NewChanObjStream(out, in))
+
+	go server.Listen()
+	go client.Listen()
+
+	return server, client
 }
