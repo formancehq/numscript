b655a89cb6be353780fd0a4802a5724597e91b6f
ascandone <alescandone@gmail.com>
2025-02-18 16:18:28 +0100

    test: test jsonrpc notifications

--- internal/jsonrpc2/jsonrpc2_test.go
+++ internal/jsonrpc2/jsonrpc2_test.go
@@ -25,6 +25,26 @@ func TestHandleRequest(t *testing.T) {
 	require.Equal(t, 142, *res)
 }
 
+func TestHandleNotification(t *testing.T) {
+	type NotifParams struct {
+		Value string `json:"value"`
+	}
+
+	server, client := newClientServer()
+
+	ch := make(chan string)
+	jsonrpc2.HandleNotification(server, "greet", func(p NotifParams) {
+		ch <- p.Value
+	})
+
+	err := jsonrpc2.SendNotification(client, "greet", NotifParams{
+		Value: "Hello!",
+	})
+	require.Nil(t, err)
+
+	require.Equal(t, "Hello!", <-ch)
+}
+
 func newClientServer() (*jsonrpc2.Server, *jsonrpc2.Server) {
 	in := make(chan jsonrpc2.Message)
 	out := make(chan jsonrpc2.Message)
