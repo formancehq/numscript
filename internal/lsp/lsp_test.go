738c5840882fea338cbab070161a4bce950ce920
ascandone <alescandone@gmail.com>
2025-02-18 15:22:53 +0100

    refactor: extract testing utility

--- internal/lsp/lsp_test.go
+++ internal/lsp/lsp_test.go
@@ -13,7 +13,7 @@ import (
 func TestServerReadWrite(t *testing.T) {
 	in := make(chan jsonrpc2.Message)
 	out := make(chan jsonrpc2.Message)
-	objStream := NewChanObjStream(in, out)
+	objStream := jsonrpc2.NewChanObjStream(in, out)
 	go lsp.RunServerWith(&objStream)
 
 	in <- jsonrpc2.Request{
@@ -34,33 +34,4 @@ func TestServerReadWrite(t *testing.T) {
 	require.Nil(t, err)
 
 	require.True(t, init.Capabilities.HoverProvider)
-}
-
-type ChanObjStream struct {
-	in  <-chan jsonrpc2.Message
-	out chan<- jsonrpc2.Message
-}
-
-var _ jsonrpc2.MessageStream = (*ChanObjStream)(nil)
-
-func NewChanObjStream(in <-chan jsonrpc2.Message, out chan<- jsonrpc2.Message) ChanObjStream {
-	return ChanObjStream{
-		in:  in,
-		out: out,
-	}
-}
-
-func (c *ChanObjStream) Close() error {
-	close(c.out)
-	return nil
-}
-
-func (c *ChanObjStream) ReadMessage() (jsonrpc2.Message, error) {
-	msg := <-c.in
-	return msg, nil
-}
-
-func (c *ChanObjStream) WriteMessage(obj jsonrpc2.Message) error {
-	c.out <- obj
-	return nil
 }
