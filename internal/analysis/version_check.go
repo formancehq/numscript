bd85c00957fdc2a6e0bdababd348dea51780684c
ascandone <alescandone@gmail.com>
2025-03-21 19:55:42 +0100

    refactor: simplify function

--- internal/analysis/version_check.go
+++ internal/analysis/version_check.go
@@ -1,6 +1,8 @@
 package analysis
 
-import "github.com/formancehq/numscript/internal/parser"
+import (
+	"github.com/formancehq/numscript/internal/parser"
+)
 
 func (res *CheckResult) checkInfixVersion(expr parser.BinaryInfix) {
 	_, isLeftANumberLit := expr.Left.(*parser.NumberLiteral)
@@ -13,41 +15,36 @@ func (res *CheckResult) checkInfixVersion(expr parser.BinaryInfix) {
 		parser.NewVersionInterpreter(0, 0, 15),
 	)
 }
+
 func (res *CheckResult) requireVersion(
 	rng parser.Range,
-	clauses ...parser.Version,
+	requiredVersion parser.Version,
 ) {
 	actualVersion := res.Program.GetVersion()
 	if actualVersion == nil {
 		return
 	}
 
-	for _, clause := range clauses {
-		switch requiredVersion := clause.(type) {
-		case parser.VersionMachine:
-			_, ok := actualVersion.(parser.VersionMachine)
-			if !ok {
+	switch requiredVersion := requiredVersion.(type) {
+	case parser.VersionMachine:
+		_, ok := actualVersion.(parser.VersionMachine)
+		if !ok {
 
-				res.pushDiagnostic(rng.GetRange(), VersionMismatch{
-					GotVersion:      actualVersion,
-					RequiredVersion: requiredVersion,
-				})
-				return
-			}
-
-		case parser.VersionInterpreter:
-			interpreterActualVersion, ok := actualVersion.(parser.VersionInterpreter)
-
-			if !ok || !interpreterActualVersion.GtEq(requiredVersion) {
-				res.pushDiagnostic(rng, VersionMismatch{
-					GotVersion:      actualVersion,
-					RequiredVersion: requiredVersion,
-				})
-				return
-			}
-
+			res.pushDiagnostic(rng.GetRange(), VersionMismatch{
+				GotVersion:      actualVersion,
+				RequiredVersion: requiredVersion,
+			})
 		}
-	}
 
-	return
+	case parser.VersionInterpreter:
+		interpreterActualVersion, ok := actualVersion.(parser.VersionInterpreter)
+
+		if !ok || !interpreterActualVersion.GtEq(requiredVersion) {
+			res.pushDiagnostic(rng, VersionMismatch{
+				GotVersion:      actualVersion,
+				RequiredVersion: requiredVersion,
+			})
+		}
+
+	}
 }
