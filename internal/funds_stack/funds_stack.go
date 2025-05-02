19bbbe9d316f75511d1a6d1ce5e69e229869587b
ascandone <alescandone@gmail.com>
2025-05-02 18:40:02 +0200

    feat: implement base funds_stack

---
+++ internal/funds_stack/funds_stack.go
@@ -0,0 +1,67 @@
+package funds_stack
+
+import (
+	"math/big"
+	"slices"
+)
+
+type Sender struct {
+	Name   string
+	Amount *big.Int
+}
+
+type FundsStack struct {
+	senders []Sender
+}
+
+func NewFundsStack(senders []Sender) FundsStack {
+	// TODO do not modify arg
+	// TODO clone big ints so that we can manipulate them
+	// TODO compact
+	slices.Reverse(senders)
+	return FundsStack{
+		senders: senders,
+	}
+}
+
+// TODO should return err as well? When not enough funds
+func (s *FundsStack) Pull(requiredAmount *big.Int) []Sender {
+	// clone so that we can manipulate this arg
+	requiredAmount = new(big.Int).Set(requiredAmount)
+
+	// TODO preallocate for perfs
+	var out []Sender
+
+	for requiredAmount.Cmp(big.NewInt(0)) != 0 {
+		if len(s.senders) == 0 {
+			panic("TODO empty senders")
+			return out
+			// TODO handle
+		}
+
+		available := s.senders[len(s.senders)-1]
+
+		switch available.Amount.Cmp(requiredAmount) {
+		case -1: // not enough:
+			out = append(out, available)
+			s.senders = s.senders[:len(s.senders)-1]
+			requiredAmount.Sub(requiredAmount, available.Amount)
+
+		default: // enough:
+			out = append(out, Sender{
+				Name:   available.Name,
+				Amount: requiredAmount,
+			})
+			available.Amount.Sub(available.Amount, requiredAmount)
+			return out
+		}
+
+	}
+
+	return out
+}
+
+// Get the total quantity of allocated funds
+func (*FundsStack) Size() *big.Int {
+	panic("TODO implement .Size()")
+}
---
+++ internal/funds_stack/funds_stack_test.go
@@ -0,0 +1,89 @@
+package funds_stack_test
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/formancehq/numscript/internal/funds_stack"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnoughBalance(t *testing.T) {
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(100)},
+	})
+
+	out := stack.Pull(big.NewInt(2))
+	require.Equal(t, []funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+	}, out)
+
+}
+
+func TestSimple(t *testing.T) {
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(10)},
+	})
+
+	out := stack.Pull(big.NewInt(5))
+	require.Equal(t, []funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(3)},
+	}, out)
+
+	out = stack.Pull(big.NewInt(7))
+	require.Equal(t, []funds_stack.Sender{
+		{Name: "s2", Amount: big.NewInt(7)},
+	}, out)
+}
+
+func TestPullZero(t *testing.T) {
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(10)},
+	})
+
+	out := stack.Pull(big.NewInt(0))
+	require.Equal(t, []funds_stack.Sender(nil), out)
+}
+
+func TestCompactFunds(t *testing.T) {
+	t.Skip("TODO")
+
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s1", Amount: big.NewInt(10)},
+	})
+
+	out := stack.Pull(big.NewInt(5))
+	require.Equal(t, []funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+	}, out)
+}
+
+func TestCompactFundsWithEmptySender(t *testing.T) {
+	t.Skip("TODO")
+
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(0)},
+		{Name: "s1", Amount: big.NewInt(10)},
+	})
+
+	out := stack.Pull(big.NewInt(5))
+	require.Equal(t, []funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+	}, out)
+}
+
+func TestMissingFunds(t *testing.T) {
+	t.Skip("TODO emit err")
+
+	stack := funds_stack.NewFundsStack([]funds_stack.Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+	})
+
+	out := stack.Pull(big.NewInt(3))
+	require.Equal(t, []funds_stack.Sender{}, out)
+}
