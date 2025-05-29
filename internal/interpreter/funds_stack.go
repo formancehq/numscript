35551b0216b770ea6d949b7057c958dd83121cf4
ascandone <alescandone@gmail.com>
2025-05-29 18:01:42 +0200

    refactor: used funds stack instead of Reconcile() function

---
+++ internal/interpreter/funds_stack.go
@@ -0,0 +1,201 @@
+package interpreter
+
+import (
+	"math/big"
+)
+
+type Sender struct {
+	Name   string
+	Amount *big.Int
+	Color  string
+}
+
+type stack[T any] struct {
+	Head T
+	Tail *stack[T]
+}
+
+func fromSlice[T any](slice []T) *stack[T] {
+	// TODO make it stack-safe
+	if len(slice) == 0 {
+		return nil
+	}
+	return &stack[T]{
+		Head: slice[0],
+		Tail: fromSlice(slice[1:]),
+	}
+}
+
+type fundsStack struct {
+	senders *stack[Sender]
+}
+
+func newFundsStack(senders []Sender) fundsStack {
+	return fundsStack{
+		senders: fromSlice(senders),
+	}
+}
+
+func (s *fundsStack) compactTop() {
+	for s.senders != nil && s.senders.Tail != nil {
+
+		first := s.senders.Head
+		second := s.senders.Tail.Head
+
+		if second.Amount.Cmp(big.NewInt(0)) == 0 {
+			s.senders = &stack[Sender]{Head: first, Tail: s.senders.Tail.Tail}
+			continue
+		}
+
+		if first.Name != second.Name || first.Color != second.Color {
+			return
+		}
+
+		s.senders = &stack[Sender]{
+			Head: Sender{
+				Name:   first.Name,
+				Color:  first.Color,
+				Amount: new(big.Int).Add(first.Amount, second.Amount),
+			},
+			Tail: s.senders.Tail.Tail,
+		}
+	}
+}
+
+func (s *fundsStack) PullAll() []Sender {
+	var senders []Sender
+	for s.senders != nil {
+		senders = append(senders, s.senders.Head)
+		s.senders = s.senders.Tail
+	}
+	return senders
+}
+
+func getLastCellOrNil(stack *stack[Sender]) *stack[Sender] {
+	for {
+		if stack.Tail == nil {
+			return stack
+		}
+		stack = stack.Tail
+	}
+}
+
+// TODO(perf) we can keep the reference of the last cell to have an O(1) push
+func (s *fundsStack) Push(senders ...Sender) {
+	newTail := fromSlice(senders)
+	if s.senders == nil {
+		s.senders = newTail
+	} else {
+		cell := getLastCellOrNil(s.senders)
+		cell.Tail = newTail
+	}
+}
+
+func (s *fundsStack) PullAnything(requiredAmount *big.Int) []Sender {
+	return s.Pull(requiredAmount, nil)
+}
+
+func (s *fundsStack) PullColored(requiredAmount *big.Int, color string) []Sender {
+	return s.Pull(requiredAmount, &color)
+}
+func (s *fundsStack) PullUncolored(requiredAmount *big.Int) []Sender {
+	return s.PullColored(requiredAmount, "")
+}
+
+func (s *fundsStack) Pull(requiredAmount *big.Int, color *string) []Sender {
+	// clone so that we can manipulate this arg
+	requiredAmount = new(big.Int).Set(requiredAmount)
+
+	// TODO preallocate for perfs
+	var out []Sender
+
+	for requiredAmount.Cmp(big.NewInt(0)) != 0 && s.senders != nil {
+		s.compactTop()
+
+		available := s.senders.Head
+		s.senders = s.senders.Tail
+
+		if color != nil && available.Color != *color {
+			out1 := s.Pull(requiredAmount, color)
+			s.senders = &stack[Sender]{
+				Head: available,
+				Tail: s.senders,
+			}
+			out = append(out, out1...)
+			break
+		}
+
+		switch available.Amount.Cmp(requiredAmount) {
+		case -1: // not enough:
+			out = append(out, available)
+			requiredAmount.Sub(requiredAmount, available.Amount)
+
+		case 1: // more than enough
+			s.senders = &stack[Sender]{
+				Head: Sender{
+					Name:   available.Name,
+					Color:  available.Color,
+					Amount: new(big.Int).Sub(available.Amount, requiredAmount),
+				},
+				Tail: s.senders,
+			}
+			fallthrough
+
+		case 0: // exactly the same
+			out = append(out, Sender{
+				Name:   available.Name,
+				Color:  available.Color,
+				Amount: new(big.Int).Set(requiredAmount),
+			})
+			return out
+		}
+
+	}
+
+	return out
+}
+
+// Treat this stack as debts and filter out senders by "repaying" debts
+func (s *fundsStack) RepayWith(credits *fundsStack, asset string) []Posting {
+	var postings []Posting
+
+	for s.senders != nil {
+		// Peek head from debts and try to pull that much
+		hd := s.senders.Head
+
+		senders := credits.Pull(hd.Amount, &hd.Color)
+		totalRepayed := big.NewInt(0)
+		for _, sender := range senders {
+			totalRepayed.Add(totalRepayed, sender.Amount)
+			postings = append(postings, Posting{
+				Source:      sender.Name,
+				Destination: hd.Name,
+				Amount:      sender.Amount,
+				Asset:       coloredAsset(asset, &sender.Color),
+			})
+		}
+
+		pulled := s.Pull(totalRepayed, &hd.Color)
+		if len(pulled) == 0 {
+			break
+		}
+
+		// careful: infinite loops possible with different colors
+		// break
+	}
+
+	return postings
+}
+
+// Clone the stack so that you can safely mutate one without mutating the other
+func (s fundsStack) Clone() fundsStack {
+	fs := newFundsStack(nil)
+
+	senders := s.senders
+	for senders != nil {
+		fs.Push(senders.Head)
+		senders = senders.Tail
+	}
+
+	return fs
+}
---
+++ internal/interpreter/funds_stack_test.go
@@ -0,0 +1,235 @@
+package interpreter
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestEnoughBalance(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(100)},
+	})
+
+	out := stack.PullAnything(big.NewInt(2))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+	}, out)
+
+}
+
+func TestPush(t *testing.T) {
+	stack := newFundsStack(nil)
+	stack.Push(Sender{Name: "acc", Amount: big.NewInt(100)})
+
+	out := stack.PullUncolored(big.NewInt(20))
+	require.Equal(t, []Sender{
+		{Name: "acc", Amount: big.NewInt(20)},
+	}, out)
+
+}
+
+func TestSimple(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(10)},
+	})
+
+	out := stack.PullAnything(big.NewInt(5))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(3)},
+	}, out)
+
+	out = stack.PullAnything(big.NewInt(7))
+	require.Equal(t, []Sender{
+		{Name: "s2", Amount: big.NewInt(7)},
+	}, out)
+}
+
+func TestPullZero(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(10)},
+	})
+
+	out := stack.PullAnything(big.NewInt(0))
+	require.Equal(t, []Sender(nil), out)
+}
+
+func TestCompactFunds(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s1", Amount: big.NewInt(10)},
+	})
+
+	out := stack.PullAnything(big.NewInt(5))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+	}, out)
+}
+
+func TestCompactFunds3Times(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s1", Amount: big.NewInt(3)},
+		{Name: "s1", Amount: big.NewInt(1)},
+	})
+
+	out := stack.PullAnything(big.NewInt(6))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(6)},
+	}, out)
+}
+
+func TestCompactFundsWithEmptySender(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+		{Name: "s2", Amount: big.NewInt(0)},
+		{Name: "s1", Amount: big.NewInt(10)},
+	})
+
+	out := stack.PullAnything(big.NewInt(5))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+	}, out)
+}
+
+func TestMissingFunds(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+	})
+
+	out := stack.PullAnything(big.NewInt(300))
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(2)},
+	}, out)
+}
+
+func TestNoZeroLeftovers(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(10)},
+		{Name: "s2", Amount: big.NewInt(15)},
+	})
+
+	stack.PullAnything(big.NewInt(10))
+
+	out := stack.PullAnything(big.NewInt(15))
+	require.Equal(t, []Sender{
+		{Name: "s2", Amount: big.NewInt(15)},
+	}, out)
+}
+
+func TestReconcileColoredManyDestPerSender(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{"src", big.NewInt(10), "X"},
+	})
+
+	out := stack.PullColored(big.NewInt(5), "X")
+	require.Equal(t, []Sender{
+		{Name: "src", Amount: big.NewInt(5), Color: "X"},
+	}, out)
+
+	out = stack.PullColored(big.NewInt(5), "X")
+	require.Equal(t, []Sender{
+		{Name: "src", Amount: big.NewInt(5), Color: "X"},
+	}, out)
+
+}
+
+func TestPullColored(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+		{Name: "s2", Amount: big.NewInt(1), Color: "red"},
+		{Name: "s3", Amount: big.NewInt(10)},
+		{Name: "s4", Amount: big.NewInt(2), Color: "red"},
+		{Name: "s5", Amount: big.NewInt(5)},
+	})
+
+	out := stack.PullColored(big.NewInt(2), "red")
+	require.Equal(t, []Sender{
+		{Name: "s2", Amount: big.NewInt(1), Color: "red"},
+		{Name: "s4", Amount: big.NewInt(1), Color: "red"},
+	}, out)
+
+	require.Equal(t, []Sender{
+		{Name: "s1", Amount: big.NewInt(5)},
+		{Name: "s3", Amount: big.NewInt(10)},
+		{Name: "s4", Amount: big.NewInt(1), Color: "red"},
+		{Name: "s5", Amount: big.NewInt(5)},
+	}, stack.PullAll())
+}
+
+func TestPullColoredComplex(t *testing.T) {
+	stack := newFundsStack([]Sender{
+		{"s1", big.NewInt(1), "c1"},
+		{"s2", big.NewInt(1), "c2"},
+	})
+
+	out := stack.PullColored(big.NewInt(1), "c2")
+	require.Equal(t, []Sender{
+		{Name: "s2", Amount: big.NewInt(1), Color: "c2"},
+	}, out)
+}
+
+func TestRepayWhenEnough(t *testing.T) {
+	credits := newFundsStack([]Sender{
+		{"s1", big.NewInt(2), ""},
+		{"s2", big.NewInt(5), ""},
+	})
+
+	debts := newFundsStack([]Sender{
+		{"d1", big.NewInt(1), ""},
+		{"d2", big.NewInt(2), ""},
+	})
+
+	postings := debts.RepayWith(&credits, "USD")
+
+	require.Equal(t, []Posting{
+		{"s1", "d1", big.NewInt(1), "USD"},
+		{"s1", "d2", big.NewInt(1), "USD"},
+		{"s2", "d2", big.NewInt(1), "USD"},
+	}, postings)
+
+	require.Equal(t, []Sender{
+		{"s2", big.NewInt(4), ""},
+	}, credits.PullAll())
+
+	require.Equal(t, []Sender(nil), debts.PullAll())
+}
+
+func TestRepayWhenNotEnough(t *testing.T) {
+	credits := newFundsStack([]Sender{
+		{"s1", big.NewInt(10), ""},
+	})
+	debts := newFundsStack([]Sender{
+		{"d1", big.NewInt(100), ""},
+	})
+
+	postings := debts.RepayWith(&credits, "USD")
+	require.Equal(t, []Posting{
+		{"s1", "d1", big.NewInt(10), "USD"},
+	}, postings)
+
+	require.Equal(t, []Sender(nil), credits.PullAll())
+	require.Equal(t, []Sender{
+		{"d1", big.NewInt(90), ""},
+	}, debts.PullAll())
+}
+
+func TestClone(t *testing.T) {
+
+	fs := newFundsStack([]Sender{
+		{"s1", big.NewInt(10), ""},
+	})
+
+	cloned := fs.Clone()
+
+	fs.PullAll()
+
+	require.Equal(t, []Sender{
+		{"s1", big.NewInt(10), ""},
+	}, cloned.PullAll())
+
+}
