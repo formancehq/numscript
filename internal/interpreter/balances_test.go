db11997a7114db0207a641e59b8e638a48124eec
ascandone <alescandone@gmail.com>
2025-06-27 16:48:55 +0200

    impl better UI for balances

--- internal/interpreter/balances_test.go
+++ internal/interpreter/balances_test.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 	"testing"
 
+	"github.com/gkampitakis/go-snaps/snaps"
 	"github.com/stretchr/testify/require"
 )
 
@@ -46,4 +47,18 @@ func TestCloneBalances(t *testing.T) {
 	fullBalance["alice"]["USD/2"].Set(big.NewInt(42))
 
 	require.Equal(t, big.NewInt(2), cloned["alice"]["USD/2"])
+}
+
+func TestPrettyPrintBalance(t *testing.T) {
+	fullBalance := Balances{
+		"alice": AccountBalance{
+			"EUR/2":    big.NewInt(1),
+			"USD/1234": big.NewInt(999999),
+		},
+		"bob": AccountBalance{
+			"BTC": big.NewInt(3),
+		},
+	}
+
+	snaps.MatchSnapshot(t, fullBalance.PrettyPrint())
 }
