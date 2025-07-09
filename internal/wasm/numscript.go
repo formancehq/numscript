ba78585e7225aacc18eaf6f87240c87bb1a19b43
ascandone <alescandone@gmail.com>
2025-07-09 11:40:30 +0200

    get vars poc

--- internal/wasm/numscript.go
+++ internal/wasm/numscript.go
@@ -23,7 +23,44 @@ func check(this js.Value, args []js.Value) any {
 	return string(ds)
 }
 
+type VarDeclaration struct {
+	Range parser.Range `json:"range"`
+	Name  string       `json:"name"`
+	Type  string       `json:"type"`
+}
+
+func getVariables(this js.Value, args []js.Value) any {
+	script := args[0].String()
+	parsed := parser.Parse(script)
+
+	var decls []VarDeclaration
+	if parsed.Value.Vars == nil {
+		return nil
+	}
+
+	for _, decl := range parsed.Value.Vars.Declarations {
+		if decl.Origin == nil {
+			decls = append(decls, VarDeclaration{
+				Range: decl.Range,
+				Name:  decl.Name.Name,
+				Type:  decl.Type.Name,
+			})
+		}
+	}
+
+	ds, err := json.Marshal(decls)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(ds)
+}
+
 func main() {
-	js.Global().Set("check", js.FuncOf(check))
+	ns := js.Global().Call("Object")
+	js.Global().Set("Numscript", ns)
+
+	ns.Set("check", js.FuncOf(check))
+	ns.Set("getVariables", js.FuncOf(getVariables))
 	select {}
 }
