d4e5423ca13565fa1dd43fa0083413446ce46058
ascandone <alescandone@gmail.com>
2025-07-18 14:26:47 +0200

    feat: change cli command args

--- internal/cmd/run.go
+++ internal/cmd/run.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
 	"os"
-	"strings"
 
-	"github.com/formancehq/numscript/internal/flags"
 	"github.com/formancehq/numscript/internal/interpreter"
 	"github.com/formancehq/numscript/internal/parser"
 
@@ -20,132 +17,62 @@ const (
 	OutputFormatJson   = "json"
 )
 
-type runArgs struct {
-	VariablesOpt string
-	BalancesOpt  string
-	MetaOpt      string
-	RawOpt       string
-	StdinFlag    bool
+type InputsFile struct {
+	FeatureFlags []string                     `json:"featureFlags"`
+	Variables    map[string]string            `json:"variables"`
+	Meta         interpreter.AccountsMetadata `json:"metadata"`
+	Balances     interpreter.Balances         `json:"balances"`
+}
+
+type RunArgs struct {
+	InputsPath   string
 	OutFormatOpt string
-	Flags        []string
 }
 
-type inputOpts struct {
-	Script    string                       `json:"script"`
-	Variables map[string]string            `json:"variables"`
-	Meta      interpreter.AccountsMetadata `json:"metadata"`
-	Balances  interpreter.Balances         `json:"balances"`
-}
-
-func (o *inputOpts) fromRaw(opts runArgs) error {
-	if opts.RawOpt == "" {
-		return nil
-	}
-
-	err := json.Unmarshal([]byte(opts.RawOpt), o)
+func run(scriptPath string, opts RunArgs) error {
+	numscriptContent, err := os.ReadFile(scriptPath)
 	if err != nil {
-		return fmt.Errorf("invalid raw input JSON: %w", err)
-	}
-	return nil
-}
-
-func (o *inputOpts) fromStdin(opts runArgs) error {
-	if !opts.StdinFlag {
-		return nil
-	}
-
-	bytes, err := io.ReadAll(os.Stdin)
-	if err != nil {
-		return fmt.Errorf("error reading from stdin: %w", err)
-	}
-
-	err = json.Unmarshal(bytes, o)
-	if err != nil {
-		return fmt.Errorf("invalid stdin JSON: %w", err)
-	}
-	return nil
-}
-
-func (o *inputOpts) fromOptions(path string, opts runArgs) error {
-	if path != "" {
-		numscriptContent, err := os.ReadFile(path)
-		if err != nil {
-			return fmt.Errorf("error reading script file: %w", err)
-		}
-		o.Script = string(numscriptContent)
-	}
-
-	if opts.BalancesOpt != "" {
-		content, err := os.ReadFile(opts.BalancesOpt)
-		if err != nil {
-			return fmt.Errorf("error reading balances file: %w", err)
-		}
-		if err := json.Unmarshal(content, &o.Balances); err != nil {
-			return fmt.Errorf("invalid balances JSON: %w", err)
-		}
-	}
-
-	if opts.MetaOpt != "" {
-		content, err := os.ReadFile(opts.MetaOpt)
-		if err != nil {
-			return fmt.Errorf("error reading metadata file: %w", err)
-		}
-		if err := json.Unmarshal(content, &o.Meta); err != nil {
-			return fmt.Errorf("invalid metadata JSON: %w", err)
-		}
-	}
-
-	if opts.VariablesOpt != "" {
-		content, err := os.ReadFile(opts.VariablesOpt)
-		if err != nil {
-			return fmt.Errorf("error reading variables file: %w", err)
-		}
-		if err := json.Unmarshal(content, &o.Variables); err != nil {
-			return fmt.Errorf("invalid variables JSON: %w", err)
-		}
-	}
-	return nil
-}
-
-func run(path string, opts runArgs) error {
-	opt := inputOpts{
-		Variables: make(map[string]string),
-		Meta:      make(interpreter.AccountsMetadata),
-		Balances:  make(interpreter.Balances),
-	}
-
-	if err := opt.fromRaw(opts); err != nil {
-		return err
-	}
-	if err := opt.fromOptions(path, opts); err != nil {
-		return err
-	}
-	if err := opt.fromStdin(opts); err != nil {
 		return err
 	}
 
-	parseResult := parser.Parse(opt.Script)
+	parseResult := parser.Parse(string(numscriptContent))
 	if len(parseResult.Errors) != 0 {
-		fmt.Fprint(os.Stderr, parser.ParseErrorsToString(parseResult.Errors, opt.Script))
+		fmt.Fprint(os.Stderr, parser.ParseErrorsToString(parseResult.Errors, string(numscriptContent)))
 		return fmt.Errorf("parsing failed")
 	}
 
+	inputsPath := opts.InputsPath
+	if inputsPath == "" {
+		inputsPath = scriptPath + ".inputs.json"
+	}
+
+	inputsContent, err := os.ReadFile(inputsPath)
+	if err != nil {
+		return err
+	}
+
+	var inputs InputsFile
+	err = json.Unmarshal(inputsContent, &inputs)
+	if err != nil {
+		return err
+	}
+
 	featureFlags := map[string]struct{}{}
-	for _, flag := range opts.Flags {
+	for _, flag := range inputs.FeatureFlags {
 		featureFlags[flag] = struct{}{}
 	}
 
-	result, err := interpreter.RunProgram(context.Background(), parseResult.Value, opt.Variables, interpreter.StaticStore{
-		Balances: opt.Balances,
-		Meta:     opt.Meta,
+	result, iErr := interpreter.RunProgram(context.Background(), parseResult.Value, inputs.Variables, interpreter.StaticStore{
+		Balances: inputs.Balances,
+		Meta:     inputs.Meta,
 	}, featureFlags)
 
-	if err != nil {
-		rng := err.GetRange()
-		fmt.Fprint(os.Stderr, err.Error())
+	if iErr != nil {
+		rng := iErr.GetRange()
+		fmt.Fprint(os.Stderr, iErr.Error())
 		if rng.Start != rng.End {
 			fmt.Fprint(os.Stderr, "\n")
-			fmt.Fprint(os.Stderr, err.GetRange().ShowOnSource(parseResult.Source))
+			fmt.Fprint(os.Stderr, iErr.GetRange().ShowOnSource(parseResult.Source))
 		}
 		return fmt.Errorf("execution failed")
 	}
@@ -183,7 +110,7 @@ func showPretty(result *interpreter.ExecutionResult) error {
 }
 
 func getRunCmd() *cobra.Command {
-	opts := runArgs{}
+	opts := RunArgs{}
 
 	cmd := cobra.Command{
 		Use:   "run",
@@ -198,20 +125,8 @@ func getRunCmd() *cobra.Command {
 		},
 	}
 
-	// Input args
-	cmd.Flags().StringVarP(&opts.VariablesOpt, "variables", "v", "", "Path of a json file containing the variables")
-	cmd.Flags().StringVarP(&opts.BalancesOpt, "balances", "b", "", "Path of a json file containing the balances")
-	cmd.Flags().StringVarP(&opts.MetaOpt, "meta", "m", "", "Path of a json file containing the accounts metadata")
-	cmd.Flags().StringVarP(&opts.RawOpt, "raw", "r", "", "Raw json input containing script, variables, balances, metadata")
-	cmd.Flags().BoolVar(&opts.StdinFlag, "stdin", false, "Take input from stdin (same format as the --raw option)")
-
-	// Feature flag
-	cmd.Flags().StringSliceVar(&opts.Flags, "flags", nil, fmt.Sprintf("the feature flags to pass to the interpreter. Currently available flags: %s",
-		strings.Join(flags.AllFlags, ", "),
-	))
-
-	// Output options
-	cmd.Flags().StringVar(&opts.OutFormatOpt, "output-format", OutputFormatPretty, "Set the output format. Available options: pretty, json.")
+	cmd.Flags().StringVar(&opts.InputsPath, "inputs", "", "Path of a json file containing the inputs")
+	cmd.Flags().StringVarP(&opts.OutFormatOpt, "output-format", "o", OutputFormatPretty, "Set the output format. Available options: pretty, json.")
 
 	return &cmd
 }
