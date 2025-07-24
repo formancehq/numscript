559426bb09c6bd50f03f72234ad3d9a5032a67be
Alessandro Scandone <alescandone@gmail.com>
2025-07-24 14:59:48 +0200

    feat: cli, change run args (#84)
    * feat: change cli command args
    
    * feat: better docs and improve err handling
    
    * fix: fix error handling
    
    * feat: added schema for inputs file
    
    * feat: improve error output
    
    * feat: more reliable exit from cli (no explicit exit() call)

--- internal/cmd/lsp.go
+++ internal/cmd/lsp.go
@@ -12,6 +12,13 @@ var lspCmd = &cobra.Command{
 	Long:   "Run the lsp server. This command is usually meant to be used for editors integration.",
 	Hidden: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return lsp.RunServer()
+		err := lsp.RunServer()
+		if err != nil {
+			cmd.SilenceErrors = true
+			cmd.SilenceUsage = true
+			return err
+		}
+
+		return nil
 	},
 }
