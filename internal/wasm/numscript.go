0f807240f9f53aa4d0b38ddd58988d8e25905dd2
ascandone <alescandone@gmail.com>
2025-07-29 14:23:59 +0200

    WIP

--- internal/wasm/numscript.go
+++ internal/wasm/numscript.go
@@ -13,154 +13,148 @@ import (
 	"github.com/formancehq/numscript/internal/parser"
 )
 
-func serialized(f func(args []js.Value) any) js.Func {
-	return js.FuncOf(
-		func(this js.Value, args []js.Value) any {
-			out := f(args)
-
-			ds, err := json.Marshal(out)
-			if err != nil {
-				panic(err)
-			}
-
-			return string(ds)
-		},
-	)
-}
-
-var currentScript *parser.Program = nil
-
-// The json represantation of both parsing and static analysis diagnostics
-type Diagnostic struct {
-	Range    parser.Range `json:"range"`
-	Message  string       `json:"message"`
-	Severity byte         `json:"severity"`
-}
-
-func analyse(args []js.Value) any {
-	var diagnostics []Diagnostic
-
-	script := args[0].String()
-	parsed := parser.Parse(script)
+func getDiagnostics(parsed parser.ParseResult) []any {
+	var diagnostics []any
 
 	for _, perr := range parsed.Errors {
-		diagnostics = append(diagnostics, Diagnostic{
-			Range:    perr.Range,
-			Message:  perr.Msg,
-			Severity: analysis.ErrorSeverity,
+		diagnostics = append(diagnostics, map[string]any{
+			"range":    castRange(perr.Range),
+			"message":  perr.Msg,
+			"severity": analysis.ErrorSeverity,
 		})
 	}
-
 	checkResult := analysis.CheckProgram(parsed.Value)
 	for _, rawDiagnostic := range checkResult.Diagnostics {
-		diagnostics = append(diagnostics, Diagnostic{
-			Range:    rawDiagnostic.Range,
-			Message:  rawDiagnostic.Kind.Message(),
-			Severity: rawDiagnostic.Kind.Severity(),
+		diagnostics = append(diagnostics, map[string]any{
+			"range":    castRange(rawDiagnostic.Range),
+			"message":  rawDiagnostic.Kind.Message(),
+			"severity": rawDiagnostic.Kind.Severity(),
 		})
 	}
 
-	currentScript = &parsed.Value
 	return diagnostics
 }
 
-type ExecutionResult struct {
-	Ok bool `json:"ok"`
+func getVariables(parsed parser.ParseResult) any {
+	var decls []any
 
-	// -- Ok: true
-
-	interpreter.ExecutionResult
-
-	// -- Ok: false
-
-	Error string       `json:"error,omitempty"`
-	Range parser.Range `json:"range,omitempty"`
-}
-
-type inputOpts struct {
-	Script    string                       `json:"script"`
-	Variables map[string]string            `json:"variables"`
-	Meta      interpreter.AccountsMetadata `json:"metadata"`
-	Balances  interpreter.Balances         `json:"balances"`
-}
-
-func run(args []js.Value) any {
-
-	inputs := args[0]
-	jsonStr := js.Global().Get("JSON").Call("stringify", inputs).String()
-
-	var opts inputOpts
-
-	err := json.Unmarshal([]byte(jsonStr), &opts)
-	if err != nil {
-		panic(err)
-	}
-
-	result, iErr := interpreter.RunProgram(
-		context.Background(),
-		*currentScript,
-		nil,
-		nil,
-		nil,
-	)
-	if iErr != nil {
-		return ExecutionResult{
-			Ok:    false,
-			Error: iErr.Error(),
-			Range: iErr.GetRange(),
-		}
-	}
-
-	return ExecutionResult{
-		Ok: true,
-		ExecutionResult: interpreter.ExecutionResult{
-			Postings:         result.Postings,
-			Metadata:         result.Metadata,
-			AccountsMetadata: result.AccountsMetadata,
-		},
-	}
-}
-
-type VarDeclaration struct {
-	Range parser.Range `json:"range"`
-	Name  string       `json:"name"`
-	Type  string       `json:"type"`
-}
-
-func getVariables(this js.Value, args []js.Value) any {
-	script := args[0].String()
-	parsed := parser.Parse(script)
-
-	var decls []VarDeclaration
 	if parsed.Value.Vars == nil {
-		return nil
+		return []any{}
 	}
 
 	for _, decl := range parsed.Value.Vars.Declarations {
+		if decl.Name == nil || decl.Type == nil {
+			continue
+		}
+
 		if decl.Origin == nil {
-			decls = append(decls, VarDeclaration{
-				Range: decl.Range,
-				Name:  decl.Name.Name,
-				Type:  decl.Type.Name,
+			decls = append(decls, map[string]any{
+				"range": castRange(decl.Range),
+				"name":  decl.Name.Name,
+				"type":  decl.Type.Name,
 			})
 		}
 	}
 
-	ds, err := json.Marshal(decls)
+	return decls
+}
+
+func getAnalysisOutput(parsed parser.ParseResult, inputs inputOpts) any {
+	diagnostics := getDiagnostics(parsed)
+
+	if len(diagnostics) != 0 {
+		return map[string]any{
+			"ok":          false,
+			"errorType":   "analysis",
+			"diagnostics": diagnostics,
+		}
+	}
+
+	featureFlags := map[string]struct{}{}
+	for _, v := range inputs.FeatureFlags {
+		featureFlags[v] = struct{}{}
+	}
+
+	result, iErr := interpreter.RunProgram(
+		context.Background(),
+		parsed.Value,
+		inputs.Variables,
+		interpreter.StaticStore{
+			Balances: inputs.Balances,
+			Meta:     inputs.Meta,
+		},
+		featureFlags,
+	)
+
+	if iErr != nil {
+		return map[string]any{
+			"ok":           false,
+			"errorType":    "execution",
+			"errorMessage": iErr.Error(),
+			"range":        castRange(iErr.GetRange()),
+		}
+	}
+
+	return map[string]any{
+		"ok":              true,
+		"executionResult": castAnything(*result),
+	}
+}
+
+func analyse(this js.Value, args []js.Value) any {
+	script := args[0].String()
+	parsed := parser.Parse(script)
+
+	// TODO we'll want to pass them as obj
+	inputs := args[1].String()
+	var opts inputOpts
+	err := json.Unmarshal([]byte(inputs), &opts)
 	if err != nil {
 		panic(err)
 	}
 
-	return string(ds)
+	return map[string]any{
+		"declaredVars": getVariables(parsed),
+		"output":       getAnalysisOutput(parsed, opts),
+	}
+}
+
+type inputOpts struct {
+	Variables    map[string]string            `json:"variables"`
+	Meta         interpreter.AccountsMetadata `json:"metadata"`
+	Balances     interpreter.Balances         `json:"balances"`
+	FeatureFlags []string                     `json:"featureFlags,omitempty"`
 }
 
 func main() {
 	ns := js.Global().Call("Object")
 	js.Global().Set("Numscript", ns)
-
-	ns.Set("analyse", serialized(analyse))
-	ns.Set("run", serialized(run))
-
-	ns.Set("getVariables", js.FuncOf(getVariables))
+	ns.Set("analyse", js.FuncOf(analyse))
 	select {}
+}
+
+// Conversion boilerplate
+func castPosition(pos parser.Position) any {
+	return map[string]any{
+		"character": pos.Character,
+		"line":      pos.Line,
+	}
+}
+
+func castRange(rng parser.Range) any {
+	return map[string]any{
+		"start": castPosition(rng.Start),
+		"end":   castPosition(rng.End),
+	}
+}
+
+func castAnything(struct_ any) any {
+	// TODO manually cast without marshaling
+	ps, err := json.Marshal(struct_)
+	if err != nil {
+		panic(err)
+	}
+	var out any
+	json.Unmarshal(ps, &out)
+	return out
 }
