0ed13f4c5dc6c92c5f2af274958b1ad31de302fe
ascandone <alescandone@gmail.com>
2025-11-03 19:03:12 +0100

    draft
    fix algo

---
+++ internal/interpreter/asset_scaling_test.go
@@ -0,0 +1,82 @@
+package interpreter
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestScalingZeroNeeded(t *testing.T) {
+	t.Skip()
+
+	// need [EUR/2 ]
+	sol := findSolution(
+		big.NewInt(0),
+		42,
+		map[int64]*big.Int{
+			2: big.NewInt(100),
+			1: big.NewInt(1),
+		})
+
+	require.Equal(t, map[int64]*big.Int{
+		42: big.NewInt(0),
+	}, sol)
+}
+
+func TestScalingSameAsset(t *testing.T) {
+	sol := findSolution(
+		// Need [EUR/2 200]
+		big.NewInt(200),
+		2,
+
+		// Have: {EUR/2: 201}
+		map[int64]*big.Int{
+			2: big.NewInt(201),
+		})
+
+	require.Equal(t, map[int64]*big.Int{
+		2: big.NewInt(200),
+	}, sol)
+}
+
+func TestScalingSolutionLowerScale(t *testing.T) {
+	sol := findSolution(
+		big.NewInt(1),
+		0,
+		map[int64]*big.Int{
+			2: big.NewInt(900),
+		})
+
+	require.Equal(t, map[int64]*big.Int{
+		2: big.NewInt(100),
+	}, sol)
+}
+
+func TestScalingSolutionHigherScale(t *testing.T) {
+	sol := findSolution(
+		// Need [EUR/2 200]
+		big.NewInt(200),
+		2,
+
+		// Have: {EUR: 4} (eq to EUR/2 400)
+		map[int64]*big.Int{
+			0: big.NewInt(4),
+		})
+
+	require.Equal(t, map[int64]*big.Int{
+		0: big.NewInt(2),
+	}, sol)
+}
+
+func TestScalingSolutionHigherScaleNoSolution(t *testing.T) {
+	sol := findSolution(
+		big.NewInt(1),
+		2,
+		map[int64]*big.Int{
+			0: big.NewInt(100),
+			1: big.NewInt(100),
+		})
+
+	require.Nil(t, sol)
+}
