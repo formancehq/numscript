c9c7b4c37de8e847eff6a10e420976d78a857ac0
ascandone <alescandone@gmail.com>
2026-05-13 17:14:44 +0200

    exposed functionalities

---
+++ accounts/accounts.go
@@ -0,0 +1,26 @@
+package accounts
+
+import "github.com/formancehq/numscript/internal/interpreter"
+
+type (
+	InvolvedAccountExpr = interpreter.InvolvedAccountExpr
+	InvolvedAccount     = interpreter.InvolvedAccount
+	InvolvedMeta        = interpreter.InvolvedMeta
+
+	AssetLiteral   = interpreter.AssetLiteral
+	AccountLiteral = interpreter.AccountLiteral
+	MakeMonetary   = interpreter.MakeMonetary
+	NumberLiteral  = interpreter.NumberLiteral
+	StringLiteral  = interpreter.StringLiteral
+	Add            = interpreter.Add
+	Sub            = interpreter.Sub
+	Div            = interpreter.Div
+	SubPrefix      = interpreter.SubPrefix
+	FnMeta         = interpreter.FnMeta
+	GetAmount      = interpreter.GetAmount
+	GetAsset       = interpreter.GetAsset
+	GetBalance     = interpreter.GetBalance
+	GetOverdraft   = interpreter.GetOverdraft
+)
+
+var IsValidCall = interpreter.IsValidCall
