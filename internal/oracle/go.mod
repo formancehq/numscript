7cc29c4efd78d3309f3a7a33146bbb466388e229
ascandone <alescandone@gmail.com>
2026-08-26 16:33:07 +0200

    feat: copy paste ledger machine

---
+++ internal/oracle/go.mod
@@ -0,0 +1,23 @@
+module github.com/formancehq/numscript/internal/oracle
+
+go 1.26.0
+
+require (
+	github.com/antlr/antlr4/runtime/Go/antlr v1.4.10
+	github.com/formancehq/go-libs/v5 v5.6.1
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	github.com/stretchr/testify v1.11.1
+)
+
+require (
+	dario.cat/mergo v1.0.2 // indirect
+	github.com/bahlo/generic-list-go v0.2.0 // indirect
+	github.com/buger/jsonparser v1.1.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/invopop/jsonschema v0.13.0 // indirect
+	github.com/mailru/easyjson v0.9.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/wk8/go-ordered-map/v2 v2.1.8 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ internal/oracle/machine/vm/store.go
@@ -0,0 +1,81 @@
+package vm
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/formancehq/go-libs/v5/pkg/types/metadata"
+)
+
+// BalanceQuery is a map of account/asset
+type BalanceQuery map[string][]string
+
+// Balances is a map of account/asset/balance
+type Balances map[string]map[string]*big.Int
+
+type Store interface {
+	GetBalances(ctx context.Context, query BalanceQuery) (Balances, error)
+	GetAccount(ctx context.Context, address string) (*Account, error)
+}
+
+type emptyStore struct{}
+
+func (e *emptyStore) GetBalances(context.Context, BalanceQuery) (Balances, error) {
+	return Balances{}, nil
+}
+
+func (e *emptyStore) GetAccount(_ context.Context, address string) (*Account, error) {
+	return &Account{
+		Address:  address,
+		Metadata: metadata.Metadata{},
+	}, nil
+}
+
+var _ Store = (*emptyStore)(nil)
+
+var EmptyStore = &emptyStore{}
+
+type AccountWithBalances struct {
+	Account
+	Balances map[string]*big.Int
+}
+
+type StaticStore map[string]*AccountWithBalances
+
+func (s StaticStore) GetBalances(_ context.Context, query BalanceQuery) (Balances, error) {
+	ret := Balances{}
+	for accountAddress, assets := range query {
+		for _, asset := range assets {
+			ret[accountAddress] = make(map[string]*big.Int)
+			account, ok := s[accountAddress]
+			if !ok {
+				ret[accountAddress] = map[string]*big.Int{
+					asset: new(big.Int),
+				}
+				continue
+			}
+			balance, ok := account.Balances[asset]
+			if !ok {
+				ret[accountAddress][asset] = new(big.Int)
+				continue
+			}
+			ret[accountAddress][asset] = balance
+		}
+	}
+
+	return ret, nil
+}
+
+func (s StaticStore) GetAccount(_ context.Context, address string) (*Account, error) {
+	account, ok := s[address]
+	if !ok {
+		return &Account{
+			Address:  address,
+			Metadata: metadata.Metadata{},
+		}, nil
+	}
+
+	return &account.Account, nil
+}
+
+var _ Store = StaticStore{}
